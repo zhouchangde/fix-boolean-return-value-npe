7168a361be4915913fbd908ca1b8418a5422a0d6
Li-saltair <huagongyilby@126.com>
2019-06-12 10:07:13 +0800

    修改Boolean的空指针 (#6)
    修改Boolean的空指针 (#6)

--- src/main/java/com/github/hcsp/datatype/Light.java
+++ src/main/java/com/github/hcsp/datatype/Light.java
@@ -8,7 +8,12 @@ public class Light {
     Boolean on;
 
     public Light(Boolean on) {
-        this.on = on;
+        if(on != null){
+            this.on = on;
+        } else {
+            this.on = false;
+        }
+
     }
 
     // 当灯亮时返回true，灭和未知状态返回false
