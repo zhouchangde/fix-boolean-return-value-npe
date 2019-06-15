7984a404400d2a21a3d3c31f96eada7247f497fc
余韵之 <36914785+Lambert1129@users.noreply.github.com>
2019-06-15 21:22:52 +0800

    updater (#11)
    updater (#11)

--- src/main/java/com/github/hcsp/datatype/Light.java
+++ src/main/java/com/github/hcsp/datatype/Light.java
@@ -12,7 +12,10 @@ public class Light {
     }
 
     // 当灯亮时返回true，灭和未知状态返回false
-    public boolean isOn() {
+    public Boolean isOn() {
+        if(on==null){
+            return false;
+        }
         return on;
     }
 }
