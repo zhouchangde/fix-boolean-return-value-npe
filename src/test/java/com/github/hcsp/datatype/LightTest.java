7e432a4d249184ebab3815674b37fcf4fba7dd6c
Bo Zhang <bo@gradle.com>
2019-06-15 18:43:07 +0800

    Update repo

--- src/test/java/com/github/hcsp/datatype/LightTest.java
+++ src/test/java/com/github/hcsp/datatype/LightTest.java
@@ -1,17 +1,16 @@
 package com.github.hcsp.datatype;
 
-import static org.hamcrest.Matchers.containsString;
-
 import com.github.blindpirate.extensions.CaptureSystemOutput;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 public class LightTest {
     @Test
     @CaptureSystemOutput
     public void printNumbersCorrectly(CaptureSystemOutput.OutputCapture capture) {
-        capture.expect(containsString("状态未知的灯亮着吗：false"));
-        capture.expect(containsString("亮着的灯亮着吗：true"));
-        capture.expect(containsString("灭了的灯亮着吗：false"));
+        capture.expect(Matchers.containsString("状态未知的灯亮着吗：false"));
+        capture.expect(Matchers.containsString("亮着的灯亮着吗：true"));
+        capture.expect(Matchers.containsString("灭了的灯亮着吗：false"));
         Main.main(null);
     }
 }
