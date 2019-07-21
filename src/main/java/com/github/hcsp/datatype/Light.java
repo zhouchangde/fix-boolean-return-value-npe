d5f85bb212ce1a1792456bd092ee07fdf9bacf33
dong <1011284295@qq.com>
2019-07-21 12:32:09 +0800

    修复返回值中出现的空指针异常 (#39)
    修复返回值中出现的空指针异常 (#39)

--- src/main/java/com/github/hcsp/datatype/Light.java
+++ src/main/java/com/github/hcsp/datatype/Light.java
@@ -13,6 +13,10 @@ public class Light {
 
     // 当灯亮时返回true，灭和未知状态返回false
     public boolean isOn() {
+        System.out.println(on);
+        if(on == null){
+            return false;
+        }
         return on;
     }
 }
