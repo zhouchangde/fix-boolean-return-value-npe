5f8d01794d1d4ec302a4a1dd7b89623da9b24db9
Mingru Li <limingru1991@gmail.com>
2019-08-07 10:59:06 +0800

    修复自动拆箱时null的报错 (#55)
    修复自动拆箱时null的报错 (#55)

--- src/main/java/com/github/hcsp/datatype/Light.java
+++ src/main/java/com/github/hcsp/datatype/Light.java
@@ -13,6 +13,9 @@ public class Light {
 
     // 当灯亮时返回true，灭和未知状态返回false
     public boolean isOn() {
-        return on;
+        if (on != null) {
+            return on;
+        }
+        return false;
     }
 }
