918564024c69673d32b6325560bdb560bbd4462d
王杰 <46479343+wangjie-fourth@users.noreply.github.com>
2019-08-13 22:01:26 +0800

    空指针异常得存在 (#59)
    空指针异常得存在 (#59)

--- src/main/java/com/github/hcsp/datatype/Light.java
+++ src/main/java/com/github/hcsp/datatype/Light.java
@@ -13,6 +13,27 @@ public class Light {
 
     // 当灯亮时返回true，灭和未知状态返回false
     public boolean isOn() {
-        return on;
+        if (this.on == null || this.on == false){
+            return false;
+        }else
+            return true;
+    }
+
+    public boolean isOn1(){
+        // 这里会报空指针异常；这是因为this.on 会去找这个值，找不到这个值，就会报异常；
+        // 同样得逻辑，isOn()就不会出错
+        if (this.on == true){
+          return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Light light = new Light(null);
+        System.out.println(light.isOn());
+
+        // 这里会报空指针异常
+        System.out.println(light.isOn1());
     }
 }
