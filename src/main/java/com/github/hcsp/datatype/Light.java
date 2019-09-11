8aeee0c058b89700a4f02c21e9d8227b8e00d64d
OrangeC <38210165+903875816@users.noreply.github.com>
2019-09-12 02:44:47 +0800

    空指针 (#71)
    空指针 (#71)

--- src/main/java/com/github/hcsp/datatype/Light.java
+++ src/main/java/com/github/hcsp/datatype/Light.java
@@ -5,14 +5,14 @@ public class Light {
     // 亮 -> true
     // 灭 -> off
     // 未知 -> null
-    Boolean on;
+    Boolean on;//这里声明了一个变量on
 
-    public Light(Boolean on) {
-        this.on = on;
+    public Light(Boolean on) {//这里自己还创造了一个构造器，并传进来一个布尔值
+        this.on = on;//将布尔变量赋值为传进来的b布尔值
     }
 
     // 当灯亮时返回true，灭和未知状态返回false
     public boolean isOn() {
-        return on;
+        return  (on==null||on==false)?false:true;//三元运算符表达式后面是不能使用return。
     }
 }
