18274a4092286678b25085090cc8489f20b117ad
xuanzebin <38971117+xuanzebin@users.noreply.github.com>
2019-09-28 16:36:20 +0800

    Xzb branch (#81)
    Xzb branch (#81)

--- src/main/java/com/github/hcsp/datatype/Light.java
+++ src/main/java/com/github/hcsp/datatype/Light.java
@@ -13,6 +13,7 @@ public class Light {
 
     // 当灯亮时返回true，灭和未知状态返回false
     public boolean isOn() {
-        return on;
+        if (this.on == null) return false;
+        return this.on;
     }
 }
