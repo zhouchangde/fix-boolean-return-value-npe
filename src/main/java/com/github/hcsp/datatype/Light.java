21d1a1eeb432abdd9ab29464d1aab32b5fdab694
kuanjiang2077 <53144803+kuanjiang2077@users.noreply.github.com>
2019-09-28 16:42:08 +0800

    Update Light.java (#82)
    Update Light.java (#82)

--- src/main/java/com/github/hcsp/datatype/Light.java
+++ src/main/java/com/github/hcsp/datatype/Light.java
@@ -8,7 +8,11 @@ public class Light {
     Boolean on;
 
     public Light(Boolean on) {
-        this.on = on;
+        if (on == null) {
+            this.on = false;
+        } else {
+            this.on = on;
+        }
     }
 
     // 当灯亮时返回true，灭和未知状态返回false
